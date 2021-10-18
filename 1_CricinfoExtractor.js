45b2f7434f45fe9e773b8e08d597ee4d2598a4a2
Rajat Verma <rajat.verma78402@gmail.com>
2021-10-18 20:49:23 +0530

    Project completed

--- 1_CricinfoExtractor.js
+++ 1_CricinfoExtractor.js
@@ -1,3 +1,8 @@
+// the purpose of this project is to extract information of worldcup 2019 from cricinfo and present
+// that in the form of excel and pdf scorecards
+// the real purpose is to learn how to extract information and get experience with js
+
+
 
 
 // node 1_CricinfoExtractor.js --excel=Worldcup.csv --dataFolder=data --source=https://www.espncricinfo.com/series/icc-cricket-world-cup-2019-1144415/match-results 
@@ -8,37 +13,45 @@ let jsdom = require("jsdom");
 let excel = require("excel4node");
 let pdf = require("pdf-lib");
 let fs = require("fs");
-//const { exec } = require("child_process");
+let path = require("path");
 
 let args = minimist(process.argv);
 
-// donwload using axios
-// read using jsdom
-// make excel using excel4node
-// make pdf using pdf-lib
+// npm init -y
+// npm install minimist
+// npm install axios
+// npm install jsdom
+// npm install excel4node
+// npm install pdf-lib
 
-let responseKaPromise = axios.get(args.source);
-responseKaPromise.then(function(response){
+let reponseKaPromise = axios.get(args.source);
+reponseKaPromise.then(function (response) {
     let html = response.data;
 
     let dom = new jsdom.JSDOM(html);
     let document = dom.window.document;
 
     let matches = [];
-    let matchScoreDivs = document.querySelectorAll("div.match-score-block");
-    for(let i = 0; i < matchScoreDivs.length; i++){
+    let matchdivs = document.querySelectorAll("div.match-score-block");
+    for (let i = 0; i < matchdivs.length; i++) {
+        let matchdiv = matchdivs[i];
         let match = {
+            t1: "",
+            t2: "",
+            t1s: "",
+            t2s: "",
+            result: ""
         };
 
-        let namePs = matchScoreDivs[i].querySelectorAll("p.name");
-        match.t1 = namePs[0].textContent;
-        match.t2 = namePs[1].textContent;
+        let teamParas = matchdiv.querySelectorAll("div.name-detail > p.name");
+        match.t1 = teamParas[0].textContent;
+        match.t2 = teamParas[1].textContent;
 
-        let scoreSpans = matchScoreDivs[i].querySelectorAll("div.score-detail > span.score");
-        if(scoreSpans.length == 2){
+        let scoreSpans = matchdiv.querySelectorAll("div.score-detail > span.score");
+        if (scoreSpans.length == 2) {
             match.t1s = scoreSpans[0].textContent;
             match.t2s = scoreSpans[1].textContent;
-        } else if(scoreSpans.length == 1){
+        } else if (scoreSpans.length == 1) {
             match.t1s = scoreSpans[0].textContent;
             match.t2s = "";
         } else {
@@ -46,101 +59,162 @@ responseKaPromise.then(function(response){
             match.t2s = "";
         }
 
-        let spanResult = matchScoreDivs[i].querySelector("div.status-text > span");
-        match.result = spanResult.textContent;
+        let resultSpan = matchdiv.querySelector("div.status-text > span");
+        match.result = resultSpan.textContent;
 
         matches.push(match);
-        // console.log(i);
     }
 
-    // console.log(matches);
-    let teams = [];
+    let matchesJSON = JSON.stringify(matches); // done
+    fs.writeFileSync("matches.json", matchesJSON, "utf-8"); // done
 
-        for(let i = 0 ; i < matches.length ; i++){
-            populateTeams(teams, matches[i]); //puts individual team in teams array if missing
-        }
+    let teams = []; // done
+    for (let i = 0; i < matches.length; i++) {
+        putTeamInTeamsArrayIfMissing(teams, matches[i]); // done
+    }
 
-        for(let i = 0 ; i < matches.length ; i++){
-            putMatchesInAppropriateTeam(teams, matches[i]); //puts individual team in teams array if missing
-        }
+    for (let i = 0; i < matches.length; i++) {
+        putMatchInAppropriateTeam(teams, matches[i]); // done
+    }
 
-    //console.log(JSON.stringify(teams));
-    let teamsJSON = JSON.stringify(teams);
-    fs.writeFileSync("teams.json", teamsJSON, "utf-8");   
+    let teamsJSON = JSON.stringify(teams); // done
+    fs.writeFileSync("teams.json", teamsJSON, "utf-8"); // done
 
-    //Here I'll create excel file 
-    createExcelfile(teams);
-    
-
-}).catch(function(err){
-    console.log(err);
+    createExcelFile(teams);
+    createFolders(teams);
 })
 
-function populateTeams(teams, match){ // from matches array to teams array
-    // let t1idx = teams.findIndex(function(team){   //A loop runs on teams array, and returns each team to function
-    //     if(team.name == match.t1){   //we check team's name is similar to t1 team of match array's element or not
-    //         return true;
-    //     } else {
-    //         return false;
-    //     }
-    // });
+function createFolders(teams) {
+    fs.mkdirSync(args.dataFolder);
+    for (let i = 0; i < teams.length; i++) {
+        let teamFN = path.join(args.dataFolder, teams[i].name);
+        fs.mkdirSync(teamFN);
+
+        for (let j = 0; j < teams[i].matches.length; j++) {
+            let matchFileName = path.join(teamFN, teams[i].matches[j].vs + ".pdf");
+            createScoreCard(teams[i].name, teams[i].matches[j], matchFileName);
+        }
+    }
+}
+
+function createScoreCard(teamName, match, matchFileName) {
+    let t1 = teamName;
+    let t2 = match.vs;
+    let t1s = match.selfScore;
+    let t2s = match.oppScore;
+    let result = match.result;
+
+    let bytesOfPDFTemplate = fs.readFileSync("Template.pdf");
+    let pdfdocKaPromise = pdf.PDFDocument.load(bytesOfPDFTemplate);
+    pdfdocKaPromise.then(function(pdfdoc){
+        let page = pdfdoc.getPage(0);
+
+        page.drawText(t1, {
+            x: 320,
+            y: 729,
+            size: 8
+        });
+        page.drawText(t2, {
+            x: 320,
+            y: 715,
+            size: 8
+        });
+        page.drawText(t1s, {
+            x: 320,
+            y: 701,
+            size: 8
+        });
+        page.drawText(t2s, {
+            x: 320,
+            y: 687,
+            size: 8
+        });
+        page.drawText(result, {
+            x: 320,
+            y: 673,
+            size: 8
+        });
+
+        let finalPDFBytesKaPromise = pdfdoc.save();
+        finalPDFBytesKaPromise.then(function(finalPDFBytes){
+            fs.writeFileSync(matchFileName, finalPDFBytes);
+        })
+    })
+}
+
+function createExcelFile(teams) {
+    let wb = new excel.Workbook();
+
+    for (let i = 0; i < teams.length; i++) {
+        let sheet = wb.addWorksheet(teams[i].name);
+
+        sheet.cell(1, 1).string("VS");
+        sheet.cell(1, 2).string("Self Score");
+        sheet.cell(1, 3).string("Opp Score");
+        sheet.cell(1, 4).string("Result");
+        for (let j = 0; j < teams[i].matches.length; j++) {
+            sheet.cell(2 + j, 1).string(teams[i].matches[j].vs);
+            sheet.cell(2 + j, 2).string(teams[i].matches[j].selfScore);
+            sheet.cell(2 + j, 3).string(teams[i].matches[j].oppScore);
+            sheet.cell(2 + j, 4).string(teams[i].matches[j].result);
+        }
+    }
+
+    wb.write(args.excel);
+}
+
+function putTeamInTeamsArrayIfMissing(teams, match) {
     let t1idx = -1;
-    for(let i = 0 ; i < teams.length ; i++){
-        if(teams[i].name == match.t1){
+    for (let i = 0; i < teams.length; i++) {
+        if (teams[i].name == match.t1) {
             t1idx = i;
             break;
         }
     }
 
-    if(t1idx == -1){
-        let team = {
+    if (t1idx == -1) {
+        teams.push({
             name: match.t1,
-            matches:[]
-        };
-        teams.push(team);
+            matches: []
+        });
     }
 
-    
     let t2idx = -1;
-    for(let i = 0 ; i < teams.length ; i++){
-        if(teams[i].name == match.t2){
+    for (let i = 0; i < teams.length; i++) {
+        if (teams[i].name == match.t2) {
             t2idx = i;
             break;
         }
     }
-    if(t2idx == -1){
-        let team = {
-            name: match.t2,
-            matches:[]
-        };
-        teams.push(team);
-    }
 
-    
+    if (t2idx == -1) {
+        teams.push({
+            name: match.t2,
+            matches: []
+        });
+    }
 }
 
-function putMatchesInAppropriateTeam(teams, match){
+function putMatchInAppropriateTeam(teams, match) {
     let t1idx = -1;
-    for(let i = 0 ; i < teams.length ; i++){
-        if(teams[i].name == match.t1){
+    for (let i = 0; i < teams.length; i++) {
+        if (teams[i].name == match.t1) {
             t1idx = i;
             break;
         }
     }
 
     let team1 = teams[t1idx];
-    //console.log(team1);
     team1.matches.push({
         vs: match.t2,
         selfScore: match.t1s,
         oppScore: match.t2s,
         result: match.result
     });
-     
 
     let t2idx = -1;
-    for(let i = 0 ; i < teams.length ; i++){
-        if(teams[i].name == match.t2){
+    for (let i = 0; i < teams.length; i++) {
+        if (teams[i].name == match.t2) {
             t2idx = i;
             break;
         }
@@ -153,29 +227,4 @@ function putMatchesInAppropriateTeam(teams, match){
         oppScore: match.t1s,
         result: match.result
     });
-
 }
-
-function createExcelfile(teams){
-    let wb = new excel.Workbook();
-
-    for(let i = 0 ; i < teams.length ; i++){
-        let sheet = wb.addWorksheet(teams[i].name);
-        
-        sheet.cell(1,1).string("VS");
-        sheet.cell(1,2).string("Self score");
-        sheet.cell(1,3).string("Opp Score");
-        sheet.cell(1,4).string("Results");
-
-        for(let j = 0 ; j < teams[i].matches.length ; j++){
-            sheet.cell(j+3,1).string(teams[i].matches[j].vs);
-            sheet.cell(j+3,2).string(teams[i].matches[j].selfScore);
-            sheet.cell(j+3,3).string(teams[i].matches[j].oppScore);
-            sheet.cell(j+3,4).string(teams[i].matches[j].result);
-        } 
-    }
-    wb.write(args.excel);
-}
-
-
-//Rajat Verma ----
